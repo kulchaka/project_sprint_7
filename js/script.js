f726e192df0381a40b6aaebb8dd9ffdf1a3241ef
Andrii <kulchaka@gmail.com>
2020-05-23 19:40:20 +0200

    Finish_JS

--- js/script.js
+++ js/script.js
@@ -10,18 +10,19 @@ const editPopUp = document.querySelector('.popup-edit');
 const closeEditPopUp = document.querySelector('.popup-edit__close');
 const popupImage = document.querySelector('.popup-image');
 const closePopupImage = document.querySelector('.popup-image__close');
-const popupImgPicture = document.querySelector('.popup-image__content');
+/*
+ Можно лучше:
+ - Убрать неиспользуемую переменную
+*/
 const inputEditName = document.querySelector('.popup-edit__input_type_name');
+const editForm = document.querySelector('.popup-edit__form');
 const inputEditDscr = document.querySelector('.popup-edit__input-dscr');
 const userName = document.querySelector('.user-info__name');
 const userInfo = document.querySelector('.user-info__job');
 const editInfoButton = document.querySelector('.popup-edit__button');
 const inputsNameEdit = document.querySelector('.popup-edit__input_type_name');
 const inputsInfoEdit = document.querySelector('.popup-edit__input-dscr');
-const errorPopupName = document.querySelector('.popup__name-error');
-const errorPopupLink = document.querySelector('.popup__link-error');
-const errorNameElem = document.querySelector('.popup-edit__name-error');
-const errorInfoElem = document.querySelector('.popup-edit__info-error');
+
 const submit = document.querySelector('#button');
 
 const errorMessages = {
@@ -29,8 +30,6 @@ const errorMessages = {
   wrongLength: 'Должно быть от 2 до 30 символов',
   wrongLink: 'Здесь должна быть ссылка'
 };
-
-
 
 //Active Button
 
@@ -51,6 +50,12 @@ document.querySelectorAll('.popup-edit__form').forEach((elem) => {
 //AddButton
 document.querySelectorAll('.popup__form').forEach((elem) => {
   elem.addEventListener('input', (event) => {
+    /*
+     Можно лучше:
+     - Удалить неиспользуемый параметр функции
+     Надо исправить:✅
+     - Функция обновления состояния кнопки должны обрабатываться вместе с валидацией инпутов.
+    */
     if (elem.checkValidity()) {
       document.querySelector('.popup__button').classList.add('popup__button_validate');
     } else {
@@ -59,72 +64,47 @@ document.querySelectorAll('.popup__form').forEach((elem) => {
   });
 });
 
-
-//Card Add Form
-inputName.addEventListener('input', (event) => {
-  field = event.target;
-  if (field.value.length == 0) {
-    field.setCustomValidity(errorMessages.empty);
-  } else if (field.value.length < 2 || field.value.length > 30) {
-    field.setCustomValidity(errorMessages.wrongLength);
-  } else {
-    field.setCustomValidity('');
-  }
-  errorPopupName.textContent = field.validationMessage;
-});
-
-inputLink.addEventListener('input', (event) => {
-  field = event.target;
-  if (field.value.length == 0) {
-    field.setCustomValidity(errorMessages.empty);
-  } else {
-    field.setCustomValidity('');
-  }
-  errorPopupLink.textContent = field.validationMessage;
-});
-
 // Edit Form
-inputsNameEdit.addEventListener('input', (event) => {
-  field = event.target;
-  // console.log(field);
+function editForms(event) {
+  /*
+    Надо исправить:✅
+    - Валидация полей должна быть описана одной функцией
+  */
+  const field = event.target;
+  // console.log(event.target.dataset.id);
+  /*
+   Надо исправить:✅
+   - Переменная объявлена без оператора const или let
+  */
 
   //Empty or Wrong Length
-  if (field.value.length == 0) {
-    field.setCustomValidity(errorMessages.empty);
-  } else if (field.value.length < 2 || field.value.length > 30) {
-    field.setCustomValidity(errorMessages.wrongLength);
+  if (event.target.dataset.id !== 'popup__link') {
+    if (field.value.length == 0) {
+      field.setCustomValidity(errorMessages.empty);
+    } else if (field.value.length < 2 || field.value.length > 30) {
+      field.setCustomValidity(errorMessages.wrongLength);
+    } else {
+      field.setCustomValidity('');
+    }
   } else {
-    field.setCustomValidity('');
+    if (field.value.length == 0) {
+      field.setCustomValidity(errorMessages.empty);
+    }
   }
 
-
-
-  errorNameElem.textContent = field.validationMessage;
-});
-
-inputsInfoEdit.addEventListener('input', (event) => {
-  field = event.target;
-  // console.log(field);
-
-  //Empty or Wrong Length
-  if (field.value.length == 0) {
-    field.setCustomValidity(errorMessages.empty);
-  } else if (field.value.length < 2 || field.value.length > 30) {
-    field.setCustomValidity(errorMessages.wrongLength);
-  } else {
-    field.setCustomValidity('');
-  }
-
-  errorInfoElem.textContent = field.validationMessage;
-});
-
+  document.querySelector(`.${event.target.dataset.id}-error`).textContent = field.validationMessage;
+};
 
 //Get Inputs Edit Name & Info
 inputEditName.setAttribute('value', `${userName.textContent}`);
 inputEditDscr.setAttribute('value', `${userInfo.textContent}`);
 
 //Add Inputs Edit Name & Info
-document.querySelector('.popup-edit__form').addEventListener('submit', (eve) => {
+function addInfoPlaceCard(eve) {
+  /*
+    Можно лучше:✅
+    - Лучше вынести document.querySelector('.popup-edit__form') в переменную
+  */
   eve.preventDefault();
 
   //Add new inputs
@@ -134,8 +114,7 @@ document.querySelector('.popup-edit__form').addEventListener('submit', (eve) => 
   //close PopUp window after click
   editPopUp.classList.remove('popup-edit_is-opened');
 
-});
-
+};
 
 // Add&Remove Like
 function addLike(event) {
@@ -175,10 +154,7 @@ const createPlaceCard = function (name, link) {
   elem.querySelector('.place-card__image').dataset.url = link;
 
   return elem.firstElementChild;
-
 };
-
-
 
 const renderPlaceCard = function (name, link) {
   const newPlaceList = createPlaceCard(name, link);
@@ -187,10 +163,14 @@ const renderPlaceCard = function (name, link) {
 };
 
 //Click PlacesList Elements
-placesList.addEventListener('click', function (event) {
+function clickCradElemnts(event) {
 
   //Open Image Pop-UP
   if (event.target.classList.contains('place-card__image')) {
+    /*
+     Отлично:
+     - используется event.target
+    */
     openPopupImg(event);
   }
 
@@ -203,50 +183,52 @@ placesList.addEventListener('click', function (event) {
   if (event.target.classList.contains('place-card__delete-icon')) {
     removeCard(event);
   }
-
-});
-
+}
 
 //Open Image Pop-UP
 function openPopupImg(event) {
   popupImage.classList.add('popup-image_is-opened');
-  // console.log(event.target.dataset.url);
+  // console.log(event.target);
   document.querySelector('.popup-image__picture').setAttribute('src', `${event.target.dataset.url}`);
-
 }
 
 //Close Image Pop-UP
-closePopupImage.addEventListener('click', () => {
+function closeImagePopup() {
   popupImage.classList.remove('popup-image_is-opened');
-});
+};
 
-//Open EditPopup
-editButton.addEventListener('click', () => {
-  editPopUp.classList.add('popup-edit_is-opened');
-});
+// Open PopUp Form
+function openPopUp(event) {
+  // console.log(event.target.dataset.id);
+  document.querySelector(`.${event.target.dataset.id}`).classList.add(`${event.target.dataset.id}_is-opened`);
+};
 
-//Close EditPopup
-closeEditPopUp.addEventListener('click', () => {
-  editPopUp.classList.remove('popup-edit_is-opened');
-  // document.querySelector('.popup-edit__form').reset();
-});
+// Close PopUp Form
+function closePopUpForm(event) {
+  // console.log(event.target.dataset.id);
+  document.querySelector(`.${event.target.dataset.id}`).classList.remove(`${event.target.dataset.id}_is-opened`);
+  document.querySelector(`.${event.target.dataset.id}__form`).reset();
 
-// Open PopUp form
-addButton.addEventListener('click', () => {
-  popApp.classList.add('popup_is-opened');
-});
+  if (event.target.dataset.id == 'popup-edit') {
+    inputEditName.setAttribute('value', `${userName.textContent}`);
+    inputEditDscr.setAttribute('value', `${userInfo.textContent}`);
+  }
+  resetInputs(event);
+};
 
-// Close PopUp form
-closeButton.addEventListener('click', () => {
-  popApp.classList.remove('popup_is-opened');
-  document.querySelector('.popup__form').reset();
-  document.querySelector('.popup__button').classList.remove('popup__button_validate');
-});
+//reset Inputs
+function resetInputs(event) {
+  // console.log(event.target.dataset.id);
+  document.querySelector(`.${event.target.dataset.id}__name-error`).textContent = '';
 
+  if (event.target.dataset.id == 'popup-edit') {
+    document.querySelector(`.${event.target.dataset.id}__info-error`).textContent = '';
+  } else {
+    document.querySelector(`.${event.target.dataset.id}__link-error`).textContent = '';
+  }
+}
 
-//Send Form 
-form.addEventListener('submit', sendForm);
-
+//Send Form Card
 function sendForm(event) {
   event.preventDefault();
 
@@ -261,13 +243,14 @@ function sendForm(event) {
   });
 
   if (isValidForm) {
-    console.log('OK');
     renderPlaceCard(inputName.value, inputLink.value);
     popApp.classList.remove('popup_is-opened');
     document.querySelector('.popup__form').reset();
     document.querySelector('.popup__button').classList.remove('popup__button_validate');
   } else {
-    console.log('NO!!!');
+    /*
+     - Убрать все console.log()
+    */
     return false;
   }
 }
@@ -282,12 +265,26 @@ function validate(element) {
     // console.log(errorElem.textContent);
     return false;
   }
-  // else if (inputLink.type !== 'url') {
-  //   errorPopupLink.textContent = errorMessages.wrongLink;
-  //   return false;
-  // }
+  /*
+   - Убрать ненужные комментарии
+  */
   return true;
 }
+
+placesList.addEventListener('click', clickCradElemnts);
+closeButton.addEventListener('click', closePopUpForm);
+addButton.addEventListener('click', openPopUp);
+closeEditPopUp.addEventListener('click', closePopUpForm);
+editButton.addEventListener('click', openPopUp);
+closePopupImage.addEventListener('click', closeImagePopup);
+
+inputsInfoEdit.addEventListener('input', editForms);
+inputsNameEdit.addEventListener('input', editForms);
+inputName.addEventListener('input', editForms);
+inputLink.addEventListener('input', editForms);
+
+editForm.addEventListener('submit', addInfoPlaceCard);
+form.addEventListener('submit', sendForm);
 
 
 function dataLoading(arr) {
@@ -297,3 +294,25 @@ function dataLoading(arr) {
 }
 
 dataLoading(initialCards);
+
+/*
+ Что понравилось:
+ - Код разбит на небольшие функции, у функций ясные имена
+ - Попап «Новое место» валидируется
+ - Используется всплытие и делегирование событий
+ - Код откомментирован
+ Можно лучше:
+ - Убрать все ненужные комментарии
+ - Хорошей практикой структурирования кода считается сначала объявление переменных, затем функций,
+ затем назначение слушателей
+ - Реализовать закрытие попапа на esc
+ - Попап обновления профиля должен принимать текущие значения при открытии
+ - Объеденить функции закрытия и открытия попапов
+ Надо исправить:
+ - //Валидация полей должна быть описана одной функцией. Назначены события инпут должны быть на две формы.✅
+ - Баг #1✅
+ 1) Открываем попап "Новое место"
+ 2) Вписываем текст в инпуты до моммента появления ошибок валидации
+ 3) Закрываем попап через крестик
+ 4) Переоткрываем попап -> ошибки валидации не ушли
+ */
